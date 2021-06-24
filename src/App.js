32aaa2200bc481e3162f27f490f1043b48105c1e
orpinelli <68570239+orpinelli@users.noreply.github.com>
2021-06-24 11:12:54 -0300

    avatar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,23 @@ export default () => {
   return(
     <div className = "app-window">
       <div className = "sidebar"> 
-      .....
+          <header>
+            <img className = "header-avatar" src = "https://www.w3schools.com/howto/img_avatar2.png" alt = "avatar" />
+            <div className = "header-buttons">
+              <div className = "header-btn">
+
+              </div>
+            </div>
+          </header>
+
+          <div className = "search">
+      .......
+          </div>
+
+          <div className = "chatlist">
+        ..........
+          </div>
+
       </div>
 
       <div className = "contentarea"> 
